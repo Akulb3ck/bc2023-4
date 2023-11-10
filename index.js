ede47d989dceb915398b152399de532c86556dc4
Akulb3ck <andriy.cymba@gmail.com>
2023-11-10 12:52:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 const xml2js = require('xml2js');
 
 const parser = new xml2js.Parser();
@@ -8,46 +8,56 @@ const builder = new xml2js.Builder();
 const host = 'localhost';
 const port = 8000;
 
-requestListener = function (req, res) {
-    fs.readFile('data.xml', 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-            return;
-        }
+const readFileAsync = async (filePath) => {
+    try {
+        return await fs.readFile(filePath, 'utf-8');
+    } catch (error) {
+        console.error(error);
+        throw new Error('Internal Server Error');
+    }
+};
 
-        parser.parseString(data, (err, result) => {
+const parseXmlAsync = async (xmlString) => {
+    return new Promise((resolve, reject) => {
+        parser.parseString(xmlString, (err, result) => {
             if (err) {
                 console.error(err);
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Internal Server Error');
-                return;
+                reject(new Error('Internal Server Error'));
+            } else {
+                resolve(result);
             }
-
-
-            let transformedData = {
-                data: {
-                    value: [],
-                },
-            };
-
-            (result.indicators.inflation || []).forEach((item) => {
-                const ku = item.ku ? item.ku[0] : null;
-                let value = item.value ? parseFloat(item.value[0]) : null;
-
-                if (ku === '13' && value && value > 5) {
-                    transformedData.data.value.push(item.value[0]);
-                }
-            });
-
-
-            const xml = builder.buildObject(transformedData);
-
-            res.writeHead(200, { 'Content-Type': 'text/xml' });
-            res.end(xml);
         });
     });
+};
+
+const requestListener = async (req, res) => {
+    try {
+        const data = await readFileAsync('data.xml');
+        const result = await parseXmlAsync(data);
+
+        const transformedData = {
+            data: {
+                value: [],
+            },
+        };
+
+        (result.indicators.inflation || []).forEach((item) => {
+            const ku = item.ku ? item.ku[0] : null;
+            const value = item.value ? parseFloat(item.value[0]) : null;
+
+            if (ku === '13' && value && value > 5) {
+                transformedData.data.value.push(item.value[0]);
+            }
+        });
+
+        const xml = builder.buildObject(transformedData);
+
+        res.writeHead(200, { 'Content-Type': 'text/xml' });
+        res.end(xml);
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(error.message);
+    }
 };
 
 const server = http.createServer(requestListener);
